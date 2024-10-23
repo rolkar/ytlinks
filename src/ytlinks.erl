8d785e5e8936a74beccf6cdbd47c87366fd49f5c
Roland Karlsson <roland@proxel.se>
2024-10-23 19:19:21 +0200

    Add parsing for shorts

--- src/ytlinks.erl
+++ src/ytlinks.erl
@@ -202,6 +202,15 @@ get_cached_channel(Url, Cache, ErrorCache) ->
     end.
 
 get_channel_remote(<<"https://www.youtube.com/watch", _/binary>> = Url) ->
+    get_channel_remote_checked(Url);
+get_channel_remote(<<"https://www.youtube.com/shorts", _/binary>> = Url) ->
+    get_channel_remote_checked(Url);
+get_channel_remote(<<"https://www.youtube.com/", _/binary>>) ->
+    {error, getting_no_youtube_video};
+get_channel_remote(_Url) ->
+    {error, getting_not_youtube}.
+
+get_channel_remote_checked(Url) ->
     case httpc:request(Url) of
 	{ok, {{_,200,"OK"}, _, Body}} ->
 	    find_channel(Body);
@@ -210,13 +219,7 @@ get_channel_remote(<<"https://www.youtube.com/watch", _/binary>> = Url) ->
 	    {error, <<"http_reply_", ReplyBinary/binary>>};
 	_ ->
 	    {error, http_failed}
-    end;
-get_channel_remote(<<"https://www.youtube.com/shorts", _/binary>>) ->
-    {error, getting_youtube_shorts};
-get_channel_remote(<<"https://www.youtube.com/", _/binary>>) ->
-    {error, getting_no_youtube_video};
-get_channel_remote(_Url) ->
-    {error, getting_not_youtube}.
+    end.
 
 parse_url_file(File) ->
     {ok, Content} = file:read_file(File),
